2d463351c2eb02b7da9da8dfd082e35f83aa58e7
BogdanATA <batameev@gmail.com>
2026-05-12 12:08:31 -0400

    created `House` class derived from the `Asset` class. `getValue` method calculates the price based on square footage, condition and lot size.

--- src/main/java/com/pluralsight/House.java
+++ src/main/java/com/pluralsight/House.java
@@ -1,4 +1,62 @@
 package com.pluralsight;
 
-public class House {
+public class House extends Asset{
+    private String address;
+    private int condition;
+    private int squareFoot;
+    private int lotSize;
+
+    public House(String description, String dateAcquired, double originalCost, String address, int condition, int squareFoot, int lotSize) {
+        super(description, dateAcquired, originalCost);
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+
+    @Override
+    public double getValue() {
+        double pricePerSquareFoot;
+
+        switch (condition) {
+            case 1 -> pricePerSquareFoot = 180;
+            case 2 -> pricePerSquareFoot = 130;
+            case 3 -> pricePerSquareFoot = 90;
+            case 4 -> pricePerSquareFoot = 80;
+            default -> pricePerSquareFoot = 0;
+        }
+        return (squareFoot * pricePerSquareFoot) + (lotSize * .25);
+    }
 }
